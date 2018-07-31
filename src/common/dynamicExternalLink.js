b54a862bdb806b1b04e0a68b49cc5616f445c12d
triste <zhenglfsir@gmail.com>
2018-07-31 20:38:21 +0800

    Incremental development

--- src/common/dynamicExternalLink.js
+++ src/common/dynamicExternalLink.js
@@ -1,21 +1,10 @@
 ;((root) => {
   const headDom = root.document.head
   const bodyDom = root.document.body
-  const links = [
-    'https://cdn.jsdelivr.net/npm/github-markdown-css@2.10.0/github-markdown.min.css',
-    'https://cdn.jsdelivr.net/npm/highlight.js@9.12.0/styles/github.css',
-    'https://cdn.jsdelivr.net/npm/katex@0.10.0-beta/dist/katex.min.css'
-  ]
   const scripts = [
     'https://cdn.jsdelivr.net/npm/highlight.js@9.12.0/lib/highlight.js',
     'https://cdn.jsdelivr.net/npm/katex@0.10.0-beta/dist/katex.min.js'
   ]
-  links.forEach((link) => {
-    const linkDom = root.document.createElement('link')
-    linkDom.rel = 'stylesheet'
-    linkDom.href = link
-    headDom.appendChild(linkDom)
-  })
   scripts.forEach((link) => {
     const scriptDom = root.document.createElement('script')
     scriptDom.src = link

5f3cc70db5b54f2805b7f7d00444fb0e1e02b8dd
triste <zhenglfsir@gmail.com>
2018-08-13 16:33:04 +0800

    Incremental development

--- src/utils/registerSW.js
+++ src/utils/registerSW.js
@@ -2,7 +2,7 @@
  * @Author: zhenglfsir@gmail.com
  * @Date: 2018-08-13 15:56:28
  * @Last Modified by: zhenglfsir@gmail.com
- * @Last Modified time: 2018-08-13 15:59:53
+ * @Last Modified time: 2018-08-13 16:33:01
  */
 import isDev from './isDev'
 
@@ -10,7 +10,7 @@ export default () => {
   return (() => {
     if ('serviceWorker' in navigator && !isDev()) {
       navigator.serviceWorker
-        .register('/sw.js')
+        .register('/dist/sw.js')
         .then(() => {
           console.log('sw registered.')
         })
